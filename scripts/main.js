16d3bd969087422a9296e522334cd48471e002c7
spicger <spicger0@gmail.com>
2024-05-27 11:58:46 +0500

    project done

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+// swiper
+
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
